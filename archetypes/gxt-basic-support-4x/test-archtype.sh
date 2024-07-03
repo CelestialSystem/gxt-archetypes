bb9d800c8ad2a7c7b917e2eb073b51bd013b90fc
Praveen Rondi <praveen.rondi@idera.com>
2024-07-03 21:28:31 +0530

    version changed

--- archetypes/gxt-basic-support-4x/test-archtype.sh
+++ archetypes/gxt-basic-support-4x/test-archtype.sh
@@ -9,7 +9,7 @@ cd target/test
 mvn archetype:generate -DarchetypeGroupId=com.sencha.gxt.archetypes \
 -DarchetypeRepository=https://oss.sonatype.org/content/repositories/snapshots \
 -DarchetypeArtifactId=gxt-basic-support-4x-archetype \
--DarchetypeVersion=1.0-SNAPSHOT \
+-DarchetypeVersion=1.1-SNAPSHOT \
 -DgroupId=com.projectname.project \
 -DartifactId=new-project-name \
 -DinteractiveMode=false
